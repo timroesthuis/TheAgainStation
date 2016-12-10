478ee4c4bbcc72f521499106307996e05bfd13d2
Robin C. Pel <robinpel@hotmail.com>
2016-12-10 14:24:39 +0100

    Re-ordered all the functions to fit the MVC model better

--- Java/GUI/src/Main.java
+++ Java/GUI/src/Main.java
@@ -7,10 +7,19 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+
+        Controller controller = new Controller();
+
+        System.out.println("stage\n");
+
+        controller.model.setTemperature(7);
+        controller.updateAll();
+
+        System.out.println("update\n");
     }
 
     public void start(Stage myStage) throws Exception {
-        Parent rootPane = FXMLLoader.load(getClass().getResource("View.fxml"));
+        Parent rootPane = FXMLLoader.load(getClass().getResource("View rev 3.fxml"));
         myStage.setTitle("The AgainStation v0.1");
         myStage.setScene(new Scene(rootPane));
         myStage.show();
--- Java/GUI/src/Model.java
+++ Java/GUI/src/Model.java
@@ -1,5 +1,55 @@
 public class Model {
+    private int temperature = 18;
+    private int humidity = 50;
+    private int airPressure = 500;
+    private int brightness = 500;
 
+    ///// Setters //////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    // All the functions and stuff
+    /* Temperature */
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+
+    /* Humidity */
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+
+    /* Air Pressure */
+    public void setAirpressure(int airPressure) {
+        this.airPressure = airPressure;
+    }
+
+    /* Brightness */
+    public void setBrightness(int brightness) {
+        this.brightness = brightness;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    ///// Getters //////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /* Temperature */
+    public int getTemperature() {
+        return temperature;
+    }
+
+    /* Humidity */
+    public int getHumidity() {
+        return humidity;
+    }
+
+    /* Air Pressure */
+    public int getAirPressure() {
+        return airPressure;
+    }
+
+    /* Brightness */
+    public int getBrightness() {
+        return brightness;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 }
